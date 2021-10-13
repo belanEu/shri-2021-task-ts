51fc901b597cdbac65c17d8b4c84c8cfd1342344
belanEu <evgeniy.belan.97@mail.ru>
2021-10-13 20:53:58 +0300

    Изменения в models

--- src/model.ts
+++ src/model.ts
@@ -1,17 +1,42 @@
 export const Reset: string = '\x1b[0m';
 
-interface StringDictionary {
-    [index: string]: string
+interface StringDictionary<T> {
+    [index: string]: T
 }
 
-export const effects: StringDictionary = {
+interface Color<T> extends StringDictionary<T> {
+    readonly black: T,
+    readonly red: T,
+    readonly green: T,
+    readonly yellow: T,
+    readonly blue: T,
+    readonly magenta: T,
+    readonly cyan: T,
+    readonly white: T
+}
+
+interface Effect<T> extends StringDictionary<T> {
+    readonly bright: T,
+    readonly dim: T,
+    readonly italic: T,
+    readonly underscore: T,
+    readonly blink: T
+}
+
+type Primitive = 'white' | 'black';
+
+export type RichColor = Color<string>;
+export type RichEffect = Effect<string>;
+export type PrimitiveColor = Color<Primitive>;
+
+export const effects: RichEffect = {
     bright: '\x1b[1m',
     dim: '\x1b[2m',
     italic: '\x1b[3m',
     underscore: '\x1b[4m',
     blink: '\x1b[5m',
 };
-export const fontColors: StringDictionary = {
+export const fontColors: RichColor = {
     black: '\x1b[30m',
     red: '\x1b[31m',
     green: '\x1b[32m',
@@ -21,7 +46,7 @@ export const fontColors: StringDictionary = {
     cyan: '\x1b[36m',
     white: '\x1b[37m',
 };
-export const backgroundColors: StringDictionary = {
+export const backgroundColors: RichColor = {
     black: '\x1b[40m',
     red: '\x1b[41m',
     green: '\x1b[42m',
@@ -31,7 +56,8 @@ export const backgroundColors: StringDictionary = {
     cyan: '\x1b[46m',
     white: '\x1b[47m',
 };
-export const contrast: StringDictionary = {
+
+export const contrast: PrimitiveColor = {
     black: 'white',
     red: 'black',
     green: 'black',
