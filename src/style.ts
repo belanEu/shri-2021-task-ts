ee0612422087350dd7928c7247a67be9ecb2b401
belanEu <evgeniy.belan.97@mail.ru>
2021-10-16 18:42:37 +0300

    Revert "Сужен тип в color"
    This reverts commit 5a8c3f1a38a77d2d708c4ad7076294b67ca4cfed.

--- src/style.ts
+++ src/style.ts
@@ -3,13 +3,9 @@ import { markdown } from './md';
 
 type colorFunctionType = typeof color;
 type colorFunctionParams = Parameters<colorFunctionType>;
-type TextOptions = Exclude<colorFunctionParams[1], undefined>;
+type IntersectionTextOptions = colorFunctionParams[1];
 
-type markdownFunctionType = typeof markdown;
-type markdownFunctionParams = Parameters<markdownFunctionType>;
-type FormTextOptions = Exclude<markdownFunctionParams[1], undefined>;
-
-export function style(text: string, options?: TextOptions & FormTextOptions): string {
+export function style(text: string, options: IntersectionTextOptions): string {
     if (text.length === 0) {
         return text;
     }
