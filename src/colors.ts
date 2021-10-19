cb97da28a0fa4a6b6fd042a67d3f3a1712e612a6
belanEu <evgeniy.belan.97@mail.ru>
2021-10-20 01:54:25 +0300

    начальная типизация colors.js

--- src/colors.ts
+++ src/colors.ts
@@ -1,44 +1,41 @@
 import { backgroundColors, effects, fontColors, Reset } from './model';
 
-type RichColorUnion = keyof typeof fontColors;
-type RichEffectUnion = keyof typeof effects;
+type BackgroundColors = typeof backgroundColors;
+type Effects = typeof effects;
+type FontColors = typeof fontColors;
 
-function addColor(text: string, color: RichColorUnion, isBackground: boolean = false): string {
+type FontColorsKeysType = keyof FontColors;
+type BackgroundColorsKeysType = keyof BackgroundColors;
+type EffectsKeysType = keyof Effects;
+
+function addColor(text: string, color: FontColorsKeysType | BackgroundColorsKeysType, isBackground: boolean = false) {
     if (isBackground) {
         return text + backgroundColors[color];
     }
     return text + fontColors[color];
 }
-function getEffects(effectList: RichEffectUnion[]): string {
+
+function getEffects(effectList: EffectsKeysType[]) {
     return effectList.map(effect => effects[effect]).join('');
 }
 
-type FormTextOptions = {
-    bold?: boolean,
-    italic?: boolean,
-    mono?: boolean,
-    link?: string
-}
-
-type TextOptions = {
-    font?: RichColorUnion,
-    effects?: RichEffectUnion[],
-    background?: RichColorUnion
+type OptionsType = {
+    font?: FontColorsKeysType,
+    background?: BackgroundColorsKeysType,
+    effects?: EffectsKeysType[]
 };
 
-type Intersection = FormTextOptions & TextOptions;
-
-export function color(text: string, options?: Intersection): string {
+export function color(text: string, options?: OptionsType) {
     const preparedText = text.replace(/ё/g, 'е');
     let result = '';
     if (options) {
-        if (options.font && fontColors[options.font]) {
+        if (options.font && options.font in fontColors) {
             result = addColor(result, options.font);
         }
-        if (options.background && backgroundColors[options.background]) {
+        if (options.background && options.background in backgroundColors) {
             result = addColor(result, options.background, true);
         }
-        if (options.effects && options.effects.every(effect => effects[effect])) {
+        if (options.effects && options.effects.every(effect => effect in effects)) {
             result += getEffects(options.effects);
         }
         result += preparedText;
