1e8e239c63a82927cd5393ae2755752f4934141c
belanEu <evgeniy.belan.97@mail.ru>
2021-10-20 01:54:25 +0300

    начальная типизация colors.js

---
+++ src/colors.ts
@@ -0,0 +1,45 @@
+import { backgroundColors, effects, fontColors, Reset } from './model';
+
+type BackgroundColors = typeof backgroundColors;
+type Effects = typeof effects;
+type FontColors = typeof fontColors;
+
+type FontColorsKeysType = keyof FontColors;
+type BackgroundColorsKeysType = keyof BackgroundColors;
+type EffectsKeysType = keyof Effects;
+
+function addColor(text: string, color: FontColorsKeysType | BackgroundColorsKeysType, isBackground: boolean = false) {
+    if (isBackground) {
+        return text + backgroundColors[color];
+    }
+    return text + fontColors[color];
+}
+function getEffects(effectList: EffectsKeysType[]) {
+    return effectList.map(effect => effects[effect]).join('');
+}
+
+type OptionsType = {
+    font?: FontColorsKeysType,
+    background?: BackgroundColorsKeysType,
+    effects?: EffectsKeysType[]
+};
+
+export function color(text: string, options?: OptionsType) {
+    const preparedText = text.replace(/ё/g, 'е');
+    let result = '';
+    if (options) {
+        if (options.font && options.font in fontColors) {
+            result = addColor(result, options.font);
+        }
+        if (options.background && options.background in backgroundColors) {
+            result = addColor(result, options.background, true);
+        }
+        if (options.effects && options.effects.every(effect => effect in effects)) {
+            result += getEffects(options.effects);
+        }
+        result += preparedText;
+        result += Reset;
+        return result;
+    }
+    return preparedText;
+}
