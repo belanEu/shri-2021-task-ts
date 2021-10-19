444d2ed03a2d4740cafa5fb23a6a3def206abb74
belanEu <evgeniy.belan.97@mail.ru>
2021-10-20 02:29:53 +0300

    типизирован style.js

--- src/style.ts
+++ src/style.ts
@@ -1,21 +1,17 @@
 import { color } from './colors';
 import { markdown } from './md';
 
-type colorFunctionType = typeof color;
-type colorFunctionParams = Parameters<colorFunctionType>;
-type IntersectionTextOptions = colorFunctionParams[1];
+type OptionsType = Exclude<Parameters<typeof color>[1], undefined>;
 
-export function style(text: string, options: IntersectionTextOptions): string {
+export function style(text: string, options: OptionsType) {
     if (text.length === 0) {
         return text;
     }
-    if (options) {
-        if ('font' in options || 'background' in options || 'effects' in options) {
-            return color(text, options);
-        }
-        if ('bold' in options || 'italic' in options || 'mono' in options || 'link' in options) {
-            return markdown(text, options);
-        }
+    if ('font' in options || 'background' in options || 'effects' in options) {
+        return color(text, options);
+    }
+    if ('bold' in options || 'italic' in options || 'mono' in options || 'link' in options) {
+        return markdown(text, options);
     }
     return text;
 }
